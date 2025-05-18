c521692812aa77e8ea187c27d3340b6031eef46c
Ashav1309 <ashav1309@gmail.com>
2025-05-18 18:49:12 +0500

    1

--- src/models/hyperparameter_tuning.py
+++ src/models/hyperparameter_tuning.py
@@ -223,11 +223,44 @@ def create_and_compile_model(params, input_shape, num_classes, class_weights):
         clipnorm=params.get('clipnorm', 1.0)
     )
     
+    # Создаем метрики для работы с последовательностями
+    class SequenceMetrics(tf.keras.metrics.Metric):
+        def __init__(self, name='sequence_metrics', **kwargs):
+            super().__init__(name=name, **kwargs)
+            self.precision = tf.keras.metrics.Precision(thresholds=0.5)
+            self.recall = tf.keras.metrics.Recall(thresholds=0.5)
+            self.f1 = tf.keras.metrics.F1Score(threshold=0.5)
+            
+        def update_state(self, y_true, y_pred, sample_weight=None):
+            # Преобразуем входные данные в 2D
+            batch_size = tf.shape(y_true)[0]
+            y_true = tf.reshape(y_true, [-1, y_true.shape[-1]])
+            y_pred = tf.reshape(y_pred, [-1, y_pred.shape[-1]])
+            
+            if sample_weight is not None:
+                sample_weight = tf.reshape(sample_weight, [-1])
+            
+            # Обновляем метрики
+            self.precision.update_state(y_true, y_pred, sample_weight)
+            self.recall.update_state(y_true, y_pred, sample_weight)
+            self.f1.update_state(y_true, y_pred, sample_weight)
+            
+        def result(self):
+            return {
+                'precision': self.precision.result(),
+                'recall': self.recall.result(),
+                'f1_score': self.f1.result()
+            }
+            
+        def reset_state(self):
+            self.precision.reset_state()
+            self.recall.reset_state()
+            self.f1.reset_state()
+    
     # Создаем метрики
     metrics = [
-        Precision(name='precision', thresholds=0.5),
-        Recall(name='recall', thresholds=0.5),
-        F1Score(name='f1_score', threshold=0.5)  # Убрал num_classes, оставил только threshold
+        'accuracy',
+        SequenceMetrics(name='sequence_metrics')
     ]
     
     # Компилируем модель с focal loss и весами классов
