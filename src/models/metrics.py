7de2809986e84b2ecedc2410d7977a161276565a
Ashav1309 <ashav1309@gmail.com>
2025-05-20 04:13:48 +0500

    1

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -4,14 +4,46 @@ from src.models.callbacks import ScalarF1Score
 
 def get_training_metrics():
     """
-    Получение метрик для обучения модели
+    Получение метрик для обучения модели.
+    Использует те же метрики, что и для подбора гиперпараметров.
     """
+    class SequenceMetrics(tf.keras.metrics.Metric):
+        def __init__(self, name='sequence_metrics', **kwargs):
+            super().__init__(name=name, **kwargs)
+            self.precision = tf.keras.metrics.Precision(thresholds=0.5)
+            self.recall = tf.keras.metrics.Recall(thresholds=0.5)
+            self.f1 = tf.keras.metrics.F1Score(threshold=0.5)
+            
+        def update_state(self, y_true, y_pred, sample_weight=None):
+            # Преобразуем входные данные в 2D
+            batch_size = tf.shape(y_true)[0]
+            y_true = tf.reshape(y_true, [-1, y_true.shape[-1]])
+            y_pred = tf.reshape(y_pred, [-1, y_pred.shape[-1]])
+            
+            if sample_weight is not None:
+                sample_weight = tf.reshape(sample_weight, [-1])
+            
+            # Обновляем метрики
+            self.precision.update_state(y_true, y_pred, sample_weight)
+            self.recall.update_state(y_true, y_pred, sample_weight)
+            self.f1.update_state(y_true, y_pred, sample_weight)
+            
+        def result(self):
+            return {
+                'precision': self.precision.result(),
+                'recall': self.recall.result(),
+                'f1_score': self.f1.result()
+            }
+            
+        def reset_state(self):
+            self.precision.reset_state()
+            self.recall.reset_state()
+            self.f1.reset_state()
+    
     return [
         'accuracy',
-        tf.keras.metrics.Precision(name='precision_element', class_id=1, thresholds=0.5),
-        tf.keras.metrics.Recall(name='recall_element', class_id=1, thresholds=0.5),
-        tf.keras.metrics.AUC(name='auc'),
-        ScalarF1Score(name='f1_action')
+        SequenceMetrics(name='sequence_metrics'),
+        ScalarF1Score(name='scalar_f1_score')
     ]
 
 def get_tuning_metrics():
